7322316c896ca9de63e29d655b34a67eb7623339
Hampton Jacobs <hammyjacobs@gmail.com>
2022-07-15 15:23:56 -0400

    nested relationships and added some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  resources :stats
-  resources :characters
   resources :users
+
+  resources :characters do
+    resources :stats
+  end
+
+  post '/auth/login', to: 'authentications#login'
+  get '/auth/verify', to: 'authentications#verify'
+
+  get '/notes', to: 'notes#get_all_notes'
+  get '/users/:user_id/notes', to: 'notes#get_user_notes' 
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
