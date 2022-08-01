0a406633878b5e1ce824ee38e33afd0d31d875da
Hampton Jacobs <hammyjacobs@gmail.com>
2022-08-01 18:21:38 -0400

    register route successful

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { verify } from './services/users'
 import Login from './screens/Login/Login';
 import Home from './screens/Home/Home';
 import CharacterContainer from './containers/CharacterContainer/CharacterContainer';
+import Register from './screens/Register/Register';
 function App() {
   const [currentUser, setCurrentUser] = useState(null)
 
@@ -20,7 +21,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Login setCurrentUser={setCurrentUser} />} />
-        <Route path="/register" element={"register page"} />
+        <Route path="/register" element={<Register />} />
         <Route path='/home' element={<Home currentUser={currentUser} />} />
         <Route path="/combat/*" element={<CharacterContainer currentUser={currentUser} />} />
       </Routes>
