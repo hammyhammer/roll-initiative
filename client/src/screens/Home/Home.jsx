2aa119c0b7142ac02448f8015e24cf5736a8e8be
Hampton Jacobs <hammyjacobs@gmail.com>
2022-08-01 17:46:58 -0400

    gettting undefined when trying to display names of characters

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -3,12 +3,24 @@ import Layout from '../../components/Layout/Layout'
 
 export default function Home(props) {
   console.log(props.currentUser)
-  console.log(props.characters)
+  console.log(props.characters.name)
   return (
     <div>
       <Layout>
         <header>Main Page</header>
-      </Layout>
-    </div>
+        <div>
+          {props.characters &&
+            props.characters.map(character => {
+              <div key={character?.id}>
+                {
+                  <p>
+                    {character.name}
+                  </p>
+                }
+              </div>
+            })}
+        </div>
+      </Layout >
+    </div >
   )
 }
