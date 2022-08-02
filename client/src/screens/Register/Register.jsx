085cf23ad3770ae30835f5b42068c777db7bfda4
Hampton Jacobs <hammyjacobs@gmail.com>
2022-08-02 11:30:10 -0400

    adding states within register

--- client/src/screens/Register/Register.jsx
+++ client/src/screens/Register/Register.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function Register() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
     <div>Register</div>
   )
