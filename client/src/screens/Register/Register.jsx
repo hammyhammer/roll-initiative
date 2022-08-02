abd27e2cb3b4d77907952b078cf7fec076415f1d
Hampton Jacobs <hammyjacobs@gmail.com>
2022-08-02 13:59:19 -0400

    added input and forms on register

--- client/src/screens/Register/Register.jsx
+++ client/src/screens/Register/Register.jsx
@@ -5,11 +5,16 @@ export default function Register() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
   return (
     <div>
       register page
       <form >
-
+        <input type="text" value={email} placeholder="email" onChange={(event) => setEmail(event.target.value)} />
+        <input type="text" value={password} placeholder="password" onChange={(event) => setPassword(event.target.value)} />
+        <button>Create Account</button>
       </form>
     </div>
   )
